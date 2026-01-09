decb488a6280828cabcafbaa06c570eaaf3b4206
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-01-09 05:34:14 -0500

    feat: implement OAuth 2.0 server metadata discovery endpoint

--- ATProtoPDS/Sources/Auth/OAuthServerMetadata.h
+++ ATProtoPDS/Sources/Auth/OAuthServerMetadata.h
@@ -1,29 +1,6 @@
 #import <Foundation/Foundation.h>
 
-NS_ASSUME_NONNULL_BEGIN
-
 @interface OAuthServerMetadata : NSObject
-
-@property (nonatomic, copy) NSString *issuer;
-@property (nonatomic, copy) NSString *authorizationEndpoint;
-@property (nonatomic, copy) NSString *tokenEndpoint;
-@property (nonatomic, copy) NSString *pushedAuthorizationRequestEndpoint;
-@property (nonatomic, strong) NSArray<NSString *> *responseTypesSupported;
-@property (nonatomic, strong) NSArray<NSString *> *grantTypesSupported;
-@property (nonatomic, strong) NSArray<NSString *> *codeChallengeMethodsSupported;
-@property (nonatomic, strong) NSArray<NSString *> *tokenEndpointAuthMethodsSupported;
-@property (nonatomic, strong) NSArray<NSString *> *tokenEndpointAuthSigningAlgValuesSupported;
-@property (nonatomic, strong) NSArray<NSString *> *scopesSupported;
-@property (nonatomic, strong) NSArray<NSString *> *dpopSigngingAlgValuesSupported;
-@property (nonatomic, assign) BOOL authorizationResponseIssParameterSupported;
-@property (nonatomic, assign) BOOL requirePushedAuthorizationRequests;
-@property (nonatomic, assign) BOOL clientIdMetadataDocumentSupported;
-@property (nonatomic, assign) BOOL requireRequestUriRegistration;
-
-+ (instancetype)defaultMetadata;
-
-- (NSDictionary *)toDictionary;
-
+@property (nonatomic, readonly) NSDictionary *metadata;
+- (instancetype)initWithBaseURL:(NSString *)baseURL;
 @end
-
-NS_ASSUME_NONNULL_END
