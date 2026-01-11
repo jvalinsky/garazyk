210a438357867be2e20c36ca276f0f7dea6a9589
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-01-10 22:09:54 -0500

    refactor: abstract networking layer for cross-platform support

---
+++ ATProtoPDS/Sources/Network/PDSNetworkTransportLinux.m
@@ -0,0 +1,68 @@
+#import "Network/PDSNetworkTransportLinux.h"
+#import <Foundation/Foundation.h>
+
+@implementation PDSNetworkTransportFactory
+
++ (id<PDSNetworkListener>)createListenerWithPort:(NSUInteger)port {
+    return [[PDSNetworkListenerLinux alloc] initWithPort:port];
+}
+
++ (id<PDSNetworkConnection>)createConnectionWithHost:(NSString *)host port:(NSUInteger)port {
+    return [[PDSNetworkConnectionLinux alloc] initWithHost:host port:port];
+}
+
+@end
+
+@implementation PDSNetworkConnectionLinux
+@synthesize stateChangedHandler = _stateChangedHandler;
+@synthesize remoteAddress = _remoteAddress;
+
+- (instancetype)initWithHost:(NSString *)host port:(NSUInteger)port {
+    self = [super init];
+    if (self) {
+        NSLog(@"PDSNetworkConnectionLinux: robust implementation not yet available");
+    }
+    return self;
+}
+
+- (void)startWithQueue:(dispatch_queue_t)queue {
+    if (self.stateChangedHandler) {
+        self.stateChangedHandler(PDSNetworkConnectionStateFailed, [NSError errorWithDomain:@"PDSNetworkTransport" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Linux transport not implemented"}]);
+    }
+}
+
+- (void)cancel {}
+
+- (void)sendData:(NSData *)data completion:(void (^ _Nullable)(NSError * _Nullable error))completion {
+    if (completion) completion([NSError errorWithDomain:@"PDSNetworkTransport" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Linux transport not implemented"}]);
+}
+
+- (void)receiveWithMinimumLength:(NSUInteger)minLength maximumLength:(NSUInteger)maxLength completion:(void (^)(NSData * _Nullable data, BOOL isComplete, NSError * _Nullable error))completion {
+    completion(nil, NO, [NSError errorWithDomain:@"PDSNetworkTransport" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Linux transport not implemented"}]);
+}
+
+@end
+
+@implementation PDSNetworkListenerLinux
+@synthesize stateChangedHandler = _stateChangedHandler;
+@synthesize newConnectionHandler = _newConnectionHandler;
+@synthesize port = _port;
+
+- (instancetype)initWithPort:(NSUInteger)port {
+    self = [super init];
+    if (self) {
+        _port = port;
+        NSLog(@"PDSNetworkListenerLinux: robust implementation not yet available");
+    }
+    return self;
+}
+
+- (void)startWithQueue:(dispatch_queue_t)queue {
+    if (self.stateChangedHandler) {
+        self.stateChangedHandler(PDSNetworkListenerStateFailed, [NSError errorWithDomain:@"PDSNetworkTransport" code:-1 userInfo:@{NSLocalizedDescriptionKey: @"Linux transport not implemented"}]);
+    }
+}
+
+- (void)cancel {}
+
+@end
