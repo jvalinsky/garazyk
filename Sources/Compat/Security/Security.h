fe972d5561da9fde93ebd3c19a05d5efa0a5c262
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-01-12 13:48:31 -0500

    fix: Add CF types and SecKeyAlgorithm to Security.h compat header
    Add stubs for CFDictionaryRef, CFStringRef, CFBridgingRelease, etc.
    Add SecKeyAlgorithm type and SecKeyCreateRandomKey stub.

--- Sources/Compat/Security/Security.h
+++ Sources/Compat/Security/Security.h
@@ -12,6 +12,17 @@
 
 typedef int32_t OSStatus;
 
+typedef struct __CFType *CFTypeRef;
+typedef struct __CFDictionary *CFDictionaryRef;
+typedef struct __CFString *CFStringRef;
+typedef struct __CFData *CFDataRef;
+typedef struct __CFBoolean *CFBooleanRef;
+typedef struct __CFAllocator *CFAllocatorRef;
+
+typedef CFTypeRef CFBridgingRelease(CFTypeRef cf);
+typedef CFTypeRef CFRetain(CFTypeRef cf);
+typedef void CFRelease(CFTypeRef cf);
+
 static inline int SecRandomCopyBytes(int *drbg, size_t count, void *bytes) {
     (void)drbg;
     arc4random_buf(bytes, count);
@@ -19,6 +30,17 @@ static inline int SecRandomCopyBytes(int *drbg, size_t count, void *bytes) {
 }
 
 typedef struct __SecKey *SecKeyRef;
+
+typedef const struct SecKeyAlgorithm *SecKeyAlgorithm;
+
+static inline SecKeyRef SecKeyCreateRandomKey(CFDictionaryRef parameters, OSStatus *error) {
+    (void)parameters;
+    (void)error;
+    return NULL;
+}
+
+#define kSecKeyAlgorithmRSASignatureMessagePSSSHA256 0
+#define kSecKeyAlgorithmECDSASignatureRFC6979SHA256 0
 
 #endif
 
