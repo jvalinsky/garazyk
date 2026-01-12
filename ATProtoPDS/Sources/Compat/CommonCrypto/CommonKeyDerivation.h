d84aeb0227b4172b9f245e732902cb0bbeb78c04
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-01-12 17:08:42 -0500

    fix: add CommonKeyDerivation shim and fix CAR endianness

---
+++ ATProtoPDS/Sources/Compat/CommonCrypto/CommonKeyDerivation.h
@@ -0,0 +1,24 @@
+#ifndef CommonKeyDerivation_h
+#define CommonKeyDerivation_h
+
+#include <stdint.h>
+#include <openssl/evp.h>
+#include <openssl/sha.h>
+#include <openssl/hmac.h>
+
+enum {
+    kCCPBKDF2 = 2,
+};
+
+enum {
+    kCCPRFHmacAlgSHA256 = 2,
+};
+
+typedef uint32_t CCPseudoRandomAlgorithm;
+
+int CCKeyDerivationPBKDF(unsigned int algorithm, const char *password, size_t passwordLen,
+                         const uint8_t *salt, size_t saltLen,
+                         CCPseudoRandomAlgorithm prf, unsigned int rounds,
+                         unsigned char *derivedKey, size_t derivedKeyLen);
+
+#endif /* CommonKeyDerivation_h */
