baa58dfb2a102d5353da91da307505aebb5af8e1
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-01-12 04:34:31 -0500

    feat: implement token refresh in OAuth2 server

--- ATProtoPDS/Tests/test_main.m
+++ ATProtoPDS/Tests/test_main.m
@@ -57,6 +57,7 @@ int main(int argc, char * argv[]) {
             @"CARInteropTests",
             @"RepoCommitTests",
             @"PDSNetworkTransportTests",
+            @"OAuth2Tests",
             @"ActorStoreTests",
             @"DatabasePoolTests",
             @"PDSControllerTests",
