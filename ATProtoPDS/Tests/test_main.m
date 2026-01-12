474224b2f2317e762d9598f459b8f777a54e1058
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-01-12 18:54:24 -0500

    fix(cmake): explicitly include XRPC tests and update test_main

--- ATProtoPDS/Tests/test_main.m
+++ ATProtoPDS/Tests/test_main.m
@@ -62,6 +62,7 @@ int main(int argc, char * argv[]) {
             @"SubscribeReposHandlerTests",
             @"GetServiceAuthMethodTests",
             @"XrpcHandlerTests",
+            @"XrpcIntegrationTests",
             @"PDSCLITests",
             @"ActorStoreTests",
             // @"PDSNewArchitectureTests",
