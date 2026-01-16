0d9c4502b22acc9e452fafb5941ab3c4cbc88df2
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-01-15 23:17:40 -0500

    Implement PLC Store Interface & Mock Implementation

---
+++ ATProtoPDS/Sources/PLC/PLCMockStore.m
@@ -0,0 +1,47 @@
+#import "PLCMockStore.h"
+
+@interface PLCMockStore ()
+@property (nonatomic, strong) NSMutableDictionary<NSString *, NSMutableArray<PLCOperation *> *> *storage;
+@property (nonatomic, strong) dispatch_queue_t queue;
+@end
+
+@implementation PLCMockStore
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _storage = [NSMutableDictionary dictionary];
+        _queue = dispatch_queue_create("com.atproto.pds.plcmockstore", DISPATCH_QUEUE_SERIAL);
+    }
+    return self;
+}
+
+- (nullable NSArray<PLCOperation *> *)getHistoryForDID:(NSString *)did error:(NSError **)error {
+    __block NSArray<PLCOperation *> *history = nil;
+    dispatch_sync(self.queue, ^{
+        history = [self.storage[did] copy];
+    });
+    return history ?: @[];
+}
+
+- (BOOL)appendOperation:(PLCOperation *)op error:(NSError **)error {
+    if (!op.did) {
+        if (error) {
+            *error = [NSError errorWithDomain:@"PLCMockStore" code:1 userInfo:@{NSLocalizedDescriptionKey: @"Operation missing DID"}];
+        }
+        return NO;
+    }
+
+    dispatch_sync(self.queue, ^{
+        NSMutableArray<PLCOperation *> *history = self.storage[op.did];
+        if (!history) {
+            history = [NSMutableArray array];
+            self.storage[op.did] = history;
+        }
+        [history addObject:op];
+    });
+
+    return YES;
+}
+
+@end
