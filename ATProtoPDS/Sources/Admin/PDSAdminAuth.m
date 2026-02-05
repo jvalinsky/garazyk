623609e6a039970e4370424fd097e27325244cd2
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-02-04 20:35:01 -0500

    what we have

--- ATProtoPDS/Sources/Admin/PDSAdminAuth.m
+++ ATProtoPDS/Sources/Admin/PDSAdminAuth.m
@@ -1,5 +1,7 @@
 #import "Admin/PDSAdminAuth.h"
 #import <CommonCrypto/CommonHMAC.h>
+#import "Auth/JWT.h"
+#import "App/PDSController.h"
 
 @implementation PDSAdminAuth
 
@@ -26,8 +28,40 @@
 
 - (BOOL)authenticateWithPassword:(NSString *)password error:(NSError **)error {
     if ([password isEqualToString:@"admin123"]) {
-        self.adminToken = [[NSUUID UUID] UUIDString];
-        return YES;
+        PDSController *controller = [PDSController sharedController];
+        if (!controller || !controller.jwtMinter) {
+            if (error) {
+                *error = [NSError errorWithDomain:@"PDSAdminAuth"
+                                             code:500
+                                         userInfo:@{NSLocalizedDescriptionKey: @"Server not initialized"}];
+            }
+            return NO;
+        }
+
+        NSString *expectedIssuer = [[NSProcessInfo processInfo] environment][@"PDS_ISSUER"] ?: @"https://pds.local:8443";
+        NSURLComponents *issuerComponents = [NSURLComponents componentsWithString:expectedIssuer];
+        NSString *issuerHost = issuerComponents.host ?: expectedIssuer;
+        NSString *adminDID = [NSString stringWithFormat:@"did:web:%@", issuerHost];
+
+        NSMutableDictionary *claims = [NSMutableDictionary dictionary];
+        claims[@"sub"] = adminDID;
+        claims[@"scope"] = @"admin";
+        claims[@"iss"] = expectedIssuer;
+        claims[@"aud"] = expectedIssuer;
+        claims[@"exp"] = @([[NSDate dateWithTimeIntervalSinceNow:3600] timeIntervalSince1970]);
+        claims[@"iat"] = @([[NSDate date] timeIntervalSince1970]);
+
+        NSString *token = [controller.jwtMinter signPayload:claims error:nil];
+        if (token) {
+            self.adminToken = token;
+            return YES;
+        }
+
+        if (error) {
+            *error = [NSError errorWithDomain:@"PDSAdminAuth"
+                                         code:500
+                                     userInfo:@{NSLocalizedDescriptionKey: @"Failed to generate admin token"}];
+        }
     }
 
     if (error) {
