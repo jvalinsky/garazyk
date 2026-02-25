ebc7e214997024c224ec66259bae76ac326841ae
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-02-25 02:18:55 -0500

    fix(mst): expose getCID: and serializeToCBOR: in MSTNode

--- ATProtoPDS/Sources/Repository/MST.h
+++ ATProtoPDS/Sources/Repository/MST.h
@@ -10,82 +10,90 @@ NS_ASSUME_NONNULL_BEGIN
 
 /*!
  @header MST.h
- 
+
  @abstract Merkle Search Tree implementation for ATProto repositories.
- 
+
  @discussion This header defines the Merkle Search Tree (MST) data structure
  used by ATProto for content-addressable record storage. The MST provides
  efficient key-value storage with cryptographic integrity guarantees through
  content addressing.
- 
+
  @copyright Copyright (c) 2025-2026 Jack Valinsky
  */
 
 /*!
  @enum MSTNodeKind
- 
+
  @abstract Specifies the type of an MST node.
- 
+
  @constant MSTNodeKindLeaf A leaf node containing key-value entries.
  @constant MSTNodeKindNonLeaf An internal node containing subtree pointers.
  */
 typedef NS_ENUM(NSUInteger, MSTNodeKind) {
-    MSTNodeKindLeaf,
-    MSTNodeKindNonLeaf
+  MSTNodeKindLeaf,
+  MSTNodeKindNonLeaf
 };
 
 /*!
  @enum MSTDiffOperationType
- 
+
  @abstract Specifies the type of change in an MST diff operation.
- 
+
  @constant MSTDiffOperationTypeAdd A new key-value pair was added.
  @constant MSTDiffOperationTypeUpdate An existing key's value was changed.
  @constant MSTDiffOperationTypeDelete A key-value pair was removed.
  */
 typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
-    MSTDiffOperationTypeAdd,
-    MSTDiffOperationTypeUpdate,
-    MSTDiffOperationTypeDelete
+  MSTDiffOperationTypeAdd,
+  MSTDiffOperationTypeUpdate,
+  MSTDiffOperationTypeDelete
 };
 
 @class MSTDiffOperation;
 
 /*!
  @class MSTDiffOperation
- 
+
  @abstract Represents a single change between two MST versions.
- 
+
  @discussion Used for sync operations to describe what changed between commits.
  For adds, oldCID is nil. For deletes, newCID is nil. For updates, both are set.
  */
 @interface MSTDiffOperation : NSObject
 
-@property (nonatomic, copy) NSString *key;
-@property (nonatomic, assign) MSTDiffOperationType type;
-@property (nonatomic, strong, nullable) CID *previousCID;
-@property (nonatomic, strong, nullable) CID *currentCID;
+@property(nonatomic, copy) NSString *key;
+@property(nonatomic, assign) MSTDiffOperationType type;
+@property(nonatomic, strong, nullable) CID *previousCID;
+@property(nonatomic, strong, nullable) CID *currentCID;
 
-+ (instancetype)addOperationWithKey:(NSString *)key currentCID:(CID *)currentCID;
-+ (instancetype)updateOperationWithKey:(NSString *)key previousCID:(CID *)previousCID currentCID:(CID *)currentCID;
-+ (instancetype)deleteOperationWithKey:(NSString *)key previousCID:(CID *)previousCID;
++ (instancetype)addOperationWithKey:(NSString *)key
+                         currentCID:(CID *)currentCID;
++ (instancetype)updateOperationWithKey:(NSString *)key
+                           previousCID:(CID *)previousCID
+                            currentCID:(CID *)currentCID;
++ (instancetype)deleteOperationWithKey:(NSString *)key
+                           previousCID:(CID *)previousCID;
 
 @end
 
 /*!
  @class MSTEntry
- 
+
  @abstract Represents a key-value entry in the MST.
  */
 @interface MSTEntry : NSObject <NSCopying>
 
-@property (nonatomic, copy, readonly) NSString *key;
-@property (nonatomic, strong, readonly) CID *valueCID;
-@property (nonatomic, copy, readonly, nullable) NSString *subKey;
+@property(nonatomic, copy, readonly) NSString *key;
+@property(nonatomic, strong, readonly) CID *valueCID;
+@property(nonatomic, copy, readonly, nullable) NSString *subKey;
 
 + (instancetype)entryWithKey:(NSString *)key valueCID:(CID *)valueCID;
-+ (instancetype)entryWithKey:(NSString *)key valueCID:(CID *)valueCID subKey:(nullable NSString *)subKey;
-- (instancetype)initWithKey:(NSString *)key valueCID:(CID *)valueCID subKey:(nullable NSString *)subKey;
++ (instancetype)entryWithKey:(NSString *)key
+                    valueCID:(CID *)valueCID
+                      subKey:(nullable NSString *)subKey;
+- (instancetype)initWithKey:(NSString *)key
+                   valueCID:(CID *)valueCID
+                     subKey:(nullable NSString *)subKey;
 
 - (NSData *)keyBytes;
 - (NSUInteger)keyLength;
@@ -95,20 +103,20 @@ typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
 
 /*!
  @class MSTNodeEntry
- 
+
  @abstract An entry within an MST node.
  */
 @interface MSTNodeEntry : NSObject
 
-@property (nonatomic, assign) NSUInteger prefixLen;
-@property (nonatomic, copy) NSData *keySuffix;
-@property (nonatomic, strong) CID *value;
-@property (nonatomic, strong, nullable) CID *tree;
+@property(nonatomic, assign) NSUInteger prefixLen;
+@property(nonatomic, copy) NSData *keySuffix;
+@property(nonatomic, strong) CID *value;
+@property(nonatomic, strong, nullable) CID *tree;
 
 + (instancetype)entryWithPrefixLen:(NSUInteger)prefixLen
                          keySuffix:(NSData *)keySuffix
                              value:(CID *)value
-                             tree:(nullable CID *)tree;
+                              tree:(nullable CID *)tree;
 
 - (NSData *)serialize;
 
@@ -116,22 +124,27 @@ typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
 
 /*!
  @class MSTNode
- 
+
  @abstract A node in the Merkle Search Tree.
  */
 @interface MSTNode : NSObject
 
-@property (nonatomic, assign, readonly) MSTNodeKind kind;
-@property (nonatomic, strong, readonly, nullable) CID *nodeHash;
-@property (nonatomic, copy, readonly) NSArray<MSTNodeEntry *> *entries;
-@property (nonatomic, strong, readonly, nullable) CID *left;
+@property(nonatomic, assign, readonly) MSTNodeKind kind;
+@property(nonatomic, strong, readonly, nullable) CID *nodeHash;
+@property(nonatomic, copy, readonly) NSArray<MSTNodeEntry *> *entries;
+@property(nonatomic, strong, readonly, nullable) CID *left;
 
 + (instancetype)leafNodeWithEntries:(NSArray<MSTNodeEntry *> *)entries;
-+ (instancetype)nonLeafNodeWithEntries:(NSArray<MSTNodeEntry *> *)entries left:(nullable CID *)left;
++ (instancetype)nonLeafNodeWithEntries:(NSArray<MSTNodeEntry *> *)entries
+                                  left:(nullable CID *)left;
 
-- (instancetype)initWithKind:(MSTNodeKind)kind entries:(NSArray<MSTNodeEntry *> *)entries left:(nullable CID *)left;
+- (instancetype)initWithKind:(MSTNodeKind)kind
+                     entries:(NSArray<MSTNodeEntry *> *)entries
+                        left:(nullable CID *)left;
 
 - (NSData *)serialize;
+- (NSData *)serializeToCBOR:(NSMapTable<MSTNode *, CID *> *)cache;
+- (CID *)getCID:(NSMapTable<MSTNode *, CID *> *)cache;
 - (NSData *)computeHash;
 - (void)setNodeHash:(CID *)hash;
 - (NSArray<MSTEntry *> *)fullEntries;
@@ -140,20 +153,22 @@ typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
 
 /*!
  @class MST
- 
+
  @abstract The Merkle Search Tree data structure.
  */
 @interface MST : NSObject
 
-@property (nonatomic, strong, readonly, nullable) CID *rootCID;
-@property (nonatomic, strong, readonly) NSData *emptyTreeHash;
+@property(nonatomic, strong, readonly, nullable) CID *rootCID;
+@property(nonatomic, strong, readonly) NSData *emptyTreeHash;
 
 - (instancetype)initWithRootCID:(nullable CID *)rootCID;
 - (instancetype)initWithRootNode:(nullable MSTNode *)rootNode;
 - (nullable CID *)get:(NSString *)key;
 - (nullable CID *)get:(NSString *)key subKey:(nullable NSString *)subKey;
 - (void)put:(NSString *)key valueCID:(CID *)valueCID;
-- (void)put:(NSString *)key valueCID:(CID *)valueCID subKey:(nullable NSString *)subKey;
+- (void)put:(NSString *)key
+    valueCID:(CID *)valueCID
+      subKey:(nullable NSString *)subKey;
 - (void)delete:(NSString *)key;
 - (void)delete:(NSString *)key subKey:(nullable NSString *)subKey;
 - (NSArray<MSTEntry *> *)allEntries;
@@ -164,13 +179,13 @@ typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
 
 /*!
  @method diffFrom:
- 
+
  @abstract Computes the differences between this tree and an older version.
- 
+
  @discussion Compares this MST with an older version and returns all changes
  (additions, updates, deletions). Used for sync operations to generate
  repository diffs for the firehose.
- 
+
  @param oldTree The older MST to compare against (may be nil for initial state).
  @return Array of MSTDiffOperation objects describing all changes.
  */
@@ -178,9 +193,9 @@ typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
 
 /*!
  @method keyDepthString:
- 
+
  @abstract Computes the depth of a key based on its SHA-256 hash.
- 
+
  @param key The key string.
  @return The computed depth (number of leading zero bits).
  */
@@ -188,9 +203,9 @@ typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
 
 /*!
  @method keyDepthBytes:
- 
+
  @abstract Computes the depth of a key based on its SHA-256 hash.
- 
+
  @param keyBytes The key as raw bytes.
  @return The computed depth (number of leading zero bits).
  */
@@ -198,9 +213,9 @@ typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
 
 /*!
  @method keyDepth:
- 
+
  @abstract Computes the depth of a key based on its SHA-256 hash.
- 
+
  @param key The key string.
  @return The computed depth (number of leading zero bits divided by 2).
  */
@@ -208,9 +223,9 @@ typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
 
 /*!
  @method getProofNodesForKey:
- 
+
  @abstract Gets the proof nodes from root to the given key.
- 
+
  @param key The key to get proof nodes for.
  @return Array of MSTNode objects forming the proof path.
  */
@@ -238,7 +253,8 @@ typedef NS_ENUM(NSUInteger, MSTDiffOperationType) {
  @param error Error pointer for traversal or callback failures.
  @return YES if traversal completed, NO if aborted due to error.
  */
-- (BOOL)enumerateNodeCARBlocksUsingBlock:(BOOL (^)(CID *cid, NSData *data, NSError **error))block
+- (BOOL)enumerateNodeCARBlocksUsingBlock:(BOOL (^)(CID *cid, NSData *data,
+                                                   NSError **error))block
                                    error:(NSError **)error;
 
 /*!
