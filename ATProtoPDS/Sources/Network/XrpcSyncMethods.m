04cb5effa7c841c5015f45d63ddf7af61faeacbb
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-02-25 03:01:32 -0500

    Fix fragile HTTP body parsing and remove debug logs

--- ATProtoPDS/Sources/Network/XrpcSyncMethods.m
+++ ATProtoPDS/Sources/Network/XrpcSyncMethods.m
@@ -1135,8 +1135,6 @@ static NSDictionary *localSyncHostEntry(PDSServiceDatabases *serviceDatabases,
   [dispatcher registerComAtprotoSyncRequestCrawl:^(HttpRequest *request,
                                                    HttpResponse *response) {
     NSDictionary *body = request.jsonBody ?: @{};
-    PDS_LOG_INFO_C(@"Sync", @"requestCrawl headers: %@", request.headers);
-    PDS_LOG_INFO_C(@"Sync", @"requestCrawl body: %@", request.jsonBody);
     NSString *hostname = body[@"hostname"];
     if (![hostname isKindOfClass:[NSString class]] ||
         [[hostname stringByTrimmingCharactersInSet:
