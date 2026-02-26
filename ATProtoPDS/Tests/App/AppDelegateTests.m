b2f3662997d9a649b1dc5242add490a726031910
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-02-26 15:49:15 -0500

    tests(app): cover launch lifecycle without UI side effects

--- ATProtoPDS/Tests/App/AppDelegateTests.m
+++ ATProtoPDS/Tests/App/AppDelegateTests.m
@@ -1,5 +1,6 @@
 #import <XCTest/XCTest.h>
 #import "App/AppDelegate.h"
+#import <objc/runtime.h>
 
 @interface AppDelegate (TestHooks)
 - (void)startServer:(id)sender;
@@ -39,7 +40,67 @@
 @interface AppDelegateTests : XCTestCase
 @end
 
+static IMP gOriginalPDSStartIMP = NULL;
+static BOOL gStubStartReturn = YES;
+static NSInteger gStubStartCallCount = 0;
+#if TARGET_OS_OSX || defined(__APPLE__)
+static IMP gOriginalSetupStatusBarIMP = NULL;
+static NSInteger gStubSetupStatusBarCallCount = 0;
+#endif
+
+static BOOL StubPDSControllerStartServerWithError(id self, SEL _cmd, NSError **error) {
+    (void)self;
+    (void)_cmd;
+    gStubStartCallCount += 1;
+    if (!gStubStartReturn && error) {
+        *error = [NSError errorWithDomain:@"AppDelegateTests"
+                                     code:42
+                                 userInfo:@{NSLocalizedDescriptionKey: @"stubbed start failure"}];
+    }
+    return gStubStartReturn;
+}
+
+#if TARGET_OS_OSX || defined(__APPLE__)
+static void StubAppDelegateSetupStatusBar(id self, SEL _cmd) {
+    (void)self;
+    (void)_cmd;
+    gStubSetupStatusBarCallCount += 1;
+}
+#endif
+
 @implementation AppDelegateTests
+
+- (void)setUp {
+    [super setUp];
+    gStubStartReturn = YES;
+    gStubStartCallCount = 0;
+
+    Method startMethod = class_getInstanceMethod([PDSController class], @selector(startServerWithError:));
+    gOriginalPDSStartIMP = method_getImplementation(startMethod);
+    method_setImplementation(startMethod, (IMP)StubPDSControllerStartServerWithError);
+#if TARGET_OS_OSX || defined(__APPLE__)
+    gStubSetupStatusBarCallCount = 0;
+    Method setupMethod = class_getInstanceMethod([AppDelegate class], @selector(setupStatusBar));
+    if (setupMethod) {
+        gOriginalSetupStatusBarIMP = method_getImplementation(setupMethod);
+        method_setImplementation(setupMethod, (IMP)StubAppDelegateSetupStatusBar);
+    }
+#endif
+}
+
+- (void)tearDown {
+    Method startMethod = class_getInstanceMethod([PDSController class], @selector(startServerWithError:));
+    if (gOriginalPDSStartIMP) {
+        method_setImplementation(startMethod, gOriginalPDSStartIMP);
+    }
+#if TARGET_OS_OSX || defined(__APPLE__)
+    Method setupMethod = class_getInstanceMethod([AppDelegate class], @selector(setupStatusBar));
+    if (setupMethod && gOriginalSetupStatusBarIMP) {
+        method_setImplementation(setupMethod, gOriginalSetupStatusBarIMP);
+    }
+#endif
+    [super tearDown];
+}
 
 - (void)testStartServerInvokesController {
     AppDelegate *delegate = [[AppDelegate alloc] init];
@@ -77,6 +138,32 @@
 
     [delegate applicationWillTerminate:nil];
     XCTAssertEqual(mock.stopCalls, 1);
+}
+
+- (void)testApplicationDidFinishLaunchingInitializesControllerAndStartsServer {
+    AppDelegate *delegate = [[AppDelegate alloc] init];
+    gStubStartReturn = YES;
+
+    [delegate applicationDidFinishLaunching:nil];
+
+    XCTAssertNotNil(delegate.pdsController);
+    XCTAssertEqual(gStubStartCallCount, 1);
+#if TARGET_OS_OSX || defined(__APPLE__)
+    XCTAssertEqual(gStubSetupStatusBarCallCount, 1);
+#endif
+}
+
+- (void)testApplicationDidFinishLaunchingStillSetsControllerWhenStartFails {
+    AppDelegate *delegate = [[AppDelegate alloc] init];
+    gStubStartReturn = NO;
+
+    [delegate applicationDidFinishLaunching:nil];
+
+    XCTAssertNotNil(delegate.pdsController);
+    XCTAssertEqual(gStubStartCallCount, 1);
+#if TARGET_OS_OSX || defined(__APPLE__)
+    XCTAssertEqual(gStubSetupStatusBarCallCount, 1);
+#endif
 }
 
 @end
