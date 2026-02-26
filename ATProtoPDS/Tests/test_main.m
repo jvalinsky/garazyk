f0b0bf60afcaace226180462b9f795a84938580b
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-02-26 15:24:16 -0500

    tests(email): re-enable keychain provider suite across restricted envs

--- ATProtoPDS/Tests/test_main.m
+++ ATProtoPDS/Tests/test_main.m
@@ -245,6 +245,7 @@ int main(int argc, char *argv[]) {
       @"FirehoseIntegrationTests",
       @"PDSReplayCacheTests",
       @"PDSEmailHTTPClientTests",
+      @"PDSKeychainSecretsProviderTests",
       @"PDSEnvironmentSecretsProviderTests",
       @"PDSSMTPEmailProviderTests",
       @"PDSMockEmailProviderTests",
