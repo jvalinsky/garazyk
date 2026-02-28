2ca216a9e36ad298ee269fbf082ad5789c0bafff
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-02-27 20:21:44 -0500

    update xrpc to match what bsky clients expect

---
+++ ATProtoPDS/Sources/AppView/BookmarkService.h
@@ -0,0 +1,38 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@class PDSDatabase;
+
+@interface BookmarkService : NSObject
+
+- (instancetype)initWithDatabase:(PDSDatabase *)database;
+
+- (nullable NSDictionary *)getBookmarksForActor:(NSString *)actorDID
+                                           limit:(NSInteger)limit
+                                          cursor:(nullable NSString *)cursor
+                                           error:(NSError **)error;
+
+- (BOOL)indexBookmark:(NSDictionary *)record
+                  did:(NSString *)did
+                  uri:(NSString *)uri
+                  cid:(nullable NSString *)cid
+                error:(NSError **)error;
+
+- (BOOL)indexBookmarkWithDid:(NSString *)did
+                subjectURI:(NSString *)subjectURI
+                subjectCID:(nullable NSString *)subjectCID
+                 createdAt:(NSString *)createdAt
+                     error:(NSError **)error;
+
+- (BOOL)unindexBookmarkWithURI:(NSString *)uri
+                           did:(NSString *)did
+                         error:(NSError **)error;
+
+- (BOOL)unindexBookmarkWithSubjectURI:(NSString *)subjectURI
+                                 did:(NSString *)did
+                               error:(NSError **)error;
+
+@end
+
+NS_ASSUME_NONNULL_END
--- ATProtoPDS/Sources/AppView/RecordLifecycleHandler.h
+++ ATProtoPDS/Sources/AppView/RecordLifecycleHandler.h
@@ -5,6 +5,7 @@
 
  @discussion Listens for PDSRecordDidChangeNotification and generates
  notifications for likes, follows, replies, mentions, reposts, and quotes.
+ Also handles indexing for bookmarks and starter packs.
 
  @copyright Copyright (c) 2025-2026 Jack Valinsky
  */
@@ -14,17 +15,21 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @class NotificationService;
+@class BookmarkService;
+@class GraphService;
 @class PDSDatabase;
 
 /*!
  @class RecordLifecycleHandler
 
- @abstract Handles record lifecycle events to generate notifications.
+ @abstract Handles record lifecycle events to generate notifications and index social data.
  */
 @interface RecordLifecycleHandler : NSObject
 
 /*! Initialize and start observing record changes. */
 - (instancetype)initWithNotificationService:(NotificationService *)notificationService
+                            bookmarkService:(BookmarkService *)bookmarkService
+                               graphService:(GraphService *)graphService
                                    database:(PDSDatabase *)database;
 
 /*! Stop observing. */
