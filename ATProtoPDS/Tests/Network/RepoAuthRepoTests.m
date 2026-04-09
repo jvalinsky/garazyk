dde4ccc3e5b4ef6cd7186ac29f0a24368a99ac67
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-04-09 01:25:36 -0400

    Implement remaining com.atproto XRPC handler coverage

--- ATProtoPDS/Tests/Network/RepoAuthRepoTests.m
+++ ATProtoPDS/Tests/Network/RepoAuthRepoTests.m
@@ -1,5 +1,6 @@
 #import "RepoAuthXrpcTestBase.h"
 #import "Database/Service/ServiceDatabases.h"
+#import "App/Services/PDSBlobService.h"
 #import "App/Services/PDSRepositoryService.h"
 
 @interface RepoAuthRepoTests : RepoAuthXrpcTestBase
@@ -45,6 +46,21 @@
     XCTAssertEqual(response.statusCode, 401);
 }
 
+- (void)testUpdateRecordReturns401WithoutAuth {
+    NSDictionary *record = @{
+        @"$type": @"app.bsky.feed.post",
+        @"text": @"update auth test",
+        @"createdAt": [self iso8601String]
+    };
+    HttpResponse *response = [self sendJsonRequestWithPath:@"/xrpc/com.atproto.repo.updateRecord"
+                                                      body:@{@"repo": self.did1,
+                                                             @"collection": @"app.bsky.feed.post",
+                                                             @"rkey": @"auth-update-test",
+                                                             @"record": record}
+                                                   headers:@{}];
+    XCTAssertEqual(response.statusCode, 401);
+}
+
 - (void)testApplyWritesReturns401WithoutAuth {
     NSDictionary *record = @{
         @"$type": @"app.bsky.feed.post",
@@ -72,6 +88,93 @@
                                                              @"collection": @"app.bsky.feed.post",
                                                              @"rkey": @"auth-mismatch",
                                                              @"record": record}
+                                                   headers:@{@"authorization": authHeader}];
+    XCTAssertEqual(response.statusCode, 403);
+}
+
+- (void)testUpdateRecordUpdatesExistingRecord {
+    NSDictionary *record = @{
+        @"$type": @"app.bsky.feed.post",
+        @"text": @"before update",
+        @"createdAt": [self iso8601String]
+    };
+    NSDictionary *created = [self.controller createRecordForDid:self.did1
+                                                     collection:@"app.bsky.feed.post"
+                                                        record:record
+                                                validationMode:PDSValidationModeRequired
+                                                         error:nil];
+    XCTAssertNotNil(created);
+    NSString *uri = created[@"uri"];
+    NSString *rkey = [[uri componentsSeparatedByString:@"/"] lastObject];
+    XCTAssertTrue(rkey.length > 0);
+
+    NSDictionary *updatedRecord = @{
+        @"$type": @"app.bsky.feed.post",
+        @"text": @"after update",
+        @"createdAt": [self iso8601String]
+    };
+    NSString *authHeader = [NSString stringWithFormat:@"Bearer %@", self.accessJwt1];
+    HttpResponse *response = [self sendJsonRequestWithPath:@"/xrpc/com.atproto.repo.updateRecord"
+                                                      body:@{@"repo": self.did1,
+                                                             @"collection": @"app.bsky.feed.post",
+                                                             @"rkey": rkey,
+                                                             @"record": updatedRecord}
+                                                   headers:@{@"authorization": authHeader}];
+    XCTAssertEqual(response.statusCode, 200);
+    NSString *expectedURI = [NSString stringWithFormat:@"at://%@/app.bsky.feed.post/%@", self.did1, rkey];
+    XCTAssertEqualObjects(response.jsonBody[@"uri"], expectedURI);
+    XCTAssertNotNil(response.jsonBody[@"cid"]);
+
+    NSDictionary *fetched = [self.controller getRecord:[NSString stringWithFormat:@"at://%@/app.bsky.feed.post/%@", self.did1, rkey]
+                                                 forDid:self.did1
+                                                  error:nil];
+    XCTAssertNotNil(fetched);
+    XCTAssertEqualObjects(fetched[@"value"][@"text"], @"after update");
+}
+
+- (void)testGetBlobReturns401WithoutAuth {
+    HttpResponse *response = [self sendGetRequestWithPath:@"/xrpc/com.atproto.repo.getBlob"
+                                               queryParams:@{@"cid": @"bafkqaaa"}
+                                                   headers:@{}];
+    XCTAssertEqual(response.statusCode, 401);
+}
+
+- (void)testGetBlobReturnsBlobForAuthorizedOwner {
+    NSData *blobData = [@"hello-repo-getBlob" dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *uploadError = nil;
+    NSDictionary *uploadResult = [self.controller.blobService uploadBlob:blobData
+                                                                   forDid:self.did1
+                                                                  mimeType:@"text/plain"
+                                                                    error:&uploadError];
+    XCTAssertNotNil(uploadResult);
+    XCTAssertNil(uploadError);
+    NSString *cid = uploadResult[@"blob"][@"ref"][@"$link"];
+    XCTAssertTrue(cid.length > 0);
+
+    NSString *authHeader = [NSString stringWithFormat:@"Bearer %@", self.accessJwt1];
+    HttpResponse *response = [self sendGetRequestWithPath:@"/xrpc/com.atproto.repo.getBlob"
+                                               queryParams:@{@"cid": cid, @"did": self.did1}
+                                                   headers:@{@"authorization": authHeader}];
+    XCTAssertEqual(response.statusCode, 200);
+
+    NSError *readError = nil;
+    NSData *responseData = [self drainResponseBody:response error:&readError];
+    XCTAssertNil(readError);
+    XCTAssertEqualObjects(responseData, blobData);
+}
+
+- (void)testGetBlobRepoMismatchForbidden {
+    NSData *blobData = [@"hello-repo-getBlob-forbidden" dataUsingEncoding:NSUTF8StringEncoding];
+    NSDictionary *uploadResult = [self.controller.blobService uploadBlob:blobData
+                                                                   forDid:self.did1
+                                                                  mimeType:@"text/plain"
+                                                                    error:nil];
+    NSString *cid = uploadResult[@"blob"][@"ref"][@"$link"];
+    XCTAssertTrue(cid.length > 0);
+
+    NSString *authHeader = [NSString stringWithFormat:@"Bearer %@", self.accessJwt1];
+    HttpResponse *response = [self sendGetRequestWithPath:@"/xrpc/com.atproto.repo.getBlob"
+                                               queryParams:@{@"cid": cid, @"did": self.did2}
                                                    headers:@{@"authorization": authHeader}];
     XCTAssertEqual(response.statusCode, 403);
 }
