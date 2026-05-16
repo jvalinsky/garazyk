ac495f6c5e0a6212c446c5a1d5c7ea3a0a8405c9
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-05-16 19:13:37 -0400

    docs: add Doxygen documentation to PDSSQLiteBlockRepository

--- Garazyk/Sources/Core/Repositories/PDSSQLiteBlockRepository.h
+++ Garazyk/Sources/Core/Repositories/PDSSQLiteBlockRepository.h
@@ -1,5 +1,10 @@
 // SPDX-FileCopyrightText: 2025-2026 Jack Valinsky
 // SPDX-License-Identifier: Unlicense OR CC0-1.0
+/**
+ * @file PDSSQLiteBlockRepository.h
+ * @abstract SQLite implementation of the block repository.
+ */
+
 #import <Foundation/Foundation.h>
 #import "Core/Repositories/PDSBlockRepository.h"
 
@@ -7,8 +12,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class PDSDatabasePool;
 
+/**
+ * @abstract SQLite-backed implementation of the block repository.
+ */
 @interface PDSSQLiteBlockRepository : NSObject <PDSBlockRepository>
 
+/**
+ * @abstract Initializes the repository with a database pool.
+ * @param databasePool The SQLite database pool for block storage.
+ */
 - (instancetype)initWithDatabasePool:(PDSDatabasePool *)databasePool;
 
 @end
