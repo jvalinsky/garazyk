c0fd959069c1a48c24e69365b72067fafad9955d
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-05-16 19:10:29 -0400

    docs: add Doxygen documentation to SQLite session and blob repository classes

--- Garazyk/Sources/Core/Repositories/PDSSQLiteSessionRepository.h
+++ Garazyk/Sources/Core/Repositories/PDSSQLiteSessionRepository.h
@@ -1,5 +1,10 @@
 // SPDX-FileCopyrightText: 2025-2026 Jack Valinsky
 // SPDX-License-Identifier: Unlicense OR CC0-1.0
+/**
+ * @file PDSSQLiteSessionRepository.h
+ * @abstract SQLite implementation of the session repository.
+ */
+
 #import <Foundation/Foundation.h>
 #import "Core/Repositories/PDSSessionRepository.h"
 
@@ -7,8 +12,15 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class PDSDatabasePool;
 
+/**
+ * @abstract SQLite-backed implementation of the session repository.
+ */
 @interface PDSSQLiteSessionRepository : NSObject <PDSSessionRepository>
 
+/**
+ * @abstract Initializes the repository with a database pool.
+ * @param servicePool The SQLite database pool for session storage.
+ */
 - (instancetype)initWithServicePool:(PDSDatabasePool *)servicePool;
 
 @end
