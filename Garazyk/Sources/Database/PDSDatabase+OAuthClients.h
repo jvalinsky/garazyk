2d648bb6cce72a90b6d7ba3fc0f0e8a8079de7c3
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-05-16 09:44:00 -0400

    oauth: improve client persistence and schema flexibility
    - Use dynamic column detection via PRAGMA table_info
    - Improve serialization of redirect URIs and grant types
    - Switch to INSERT OR REPLACE for more robust client creation

--- Garazyk/Sources/Database/PDSDatabase+OAuthClients.h
+++ Garazyk/Sources/Database/PDSDatabase+OAuthClients.h
@@ -6,8 +6,10 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface PDSDatabase (OAuthClients)
 
-- (NSDictionary *)getClientWithID:(NSString *)clientID error:(NSError **)error;
+- (nullable NSDictionary *)getClientWithID:(NSString *)clientID error:(NSError **)error;
 - (BOOL)createClient:(NSDictionary *)client error:(NSError **)error;
+- (nullable NSDictionary *)getOAuthClientWithID:(NSString *)clientID error:(NSError **)error;
+- (BOOL)createOAuthClient:(NSDictionary *)client error:(NSError **)error;
 - (BOOL)seedTestClient:(NSError **)error;
 - (nullable NSArray<NSDictionary *> *)getAllOAuthClientsWithError:(NSError **)error;
 - (BOOL)deleteOAuthClientWithID:(NSString *)clientID error:(NSError **)error;
