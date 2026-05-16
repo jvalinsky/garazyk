b5f5ee117cfe257cd96d2b053933e0a128750d06
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-05-16 19:11:08 -0400

    docs: add Doxygen documentation to PDSLegacy repository classes

--- Garazyk/Sources/Core/Repositories/PDSLegacySessionRepository.h
+++ Garazyk/Sources/Core/Repositories/PDSLegacySessionRepository.h
@@ -1,20 +1,27 @@
 // SPDX-FileCopyrightText: 2025-2026 Jack Valinsky
 // SPDX-License-Identifier: Unlicense OR CC0-1.0
-/*!
- @file PDSLegacySessionRepository.h
- @abstract Adapter for legacy session data.
- @copyright Copyright (c) 2025 Jack Valinsky
+/**
+ * @file PDSLegacySessionRepository.h
+ * @abstract Adapter for legacy session data.
  */
 
 #import <Foundation/Foundation.h>
-#import "PDSSessionRepository.h"
+#import "Core/Repositories/PDSSessionRepository.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @class PDSServiceDatabases;
 
+/**
+ * @abstract Adapter for legacy session storage backends.
+ */
 @interface PDSLegacySessionRepository : NSObject <PDSSessionRepository>
 
+/**
+ * @abstract Initializes the repository with service databases.
+ * @param serviceDatabases The service database registry.
+ * @return An initialized repository instance.
+ */
 - (instancetype)initWithServiceDatabases:(PDSServiceDatabases *)serviceDatabases;
 
 @end
