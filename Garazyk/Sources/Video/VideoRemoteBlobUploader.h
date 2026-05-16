1d88922d0d8cdf502dfaf184ba83e0fad62690c2
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-05-16 19:01:04 -0400

    docs: add Doxygen documentation to Video Blob Uploader classes

--- Garazyk/Sources/Video/VideoRemoteBlobUploader.h
+++ Garazyk/Sources/Video/VideoRemoteBlobUploader.h
@@ -5,10 +5,20 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * @abstract Implementation of VideoBlobUploader for remote PDS blob services.
+ */
 @interface VideoRemoteBlobUploader : NSObject <VideoBlobUploader>
 
+/**
+ * @abstract The base URL of the remote PDS blob service.
+ */
 @property (nonatomic, copy, readonly) NSString *pdsURL;
 
+/**
+ * @abstract Initializes a new remote blob uploader.
+ * @param pdsURL The base URL for the PDS blob service.
+ */
 - (instancetype)initWithPDSURL:(NSString *)pdsURL;
 
 @end
