4878daa083f91637bab2b822f573758a5fd4fc09
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-05-16 19:11:08 -0400

    docs: add Doxygen documentation to PDSLegacy repository classes

--- Garazyk/Sources/Core/Repositories/PDSLegacyAccountRepository.h
+++ Garazyk/Sources/Core/Repositories/PDSLegacyAccountRepository.h
@@ -1,20 +1,27 @@
 // SPDX-FileCopyrightText: 2025-2026 Jack Valinsky
 // SPDX-License-Identifier: Unlicense OR CC0-1.0
-/*!
- @file PDSLegacyAccountRepository.h
- @abstract Adapter class for legacy account storage.
- @copyright Copyright (c) 2025 Jack Valinsky
+/**
+ * @file PDSLegacyAccountRepository.h
+ * @abstract Adapter class for legacy account storage.
  */
 
 #import <Foundation/Foundation.h>
-#import "PDSAccountRepository.h"
+#import "Core/Repositories/PDSAccountRepository.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @class PDSServiceDatabases;
 
+/**
+ * @abstract Adapter for legacy account storage backends.
+ */
 @interface PDSLegacyAccountRepository : NSObject <PDSAccountRepository>
 
+/**
+ * @abstract Initializes the repository with service databases.
+ * @param serviceDatabases The service database registry.
+ * @return An initialized repository instance.
+ */
 - (instancetype)initWithServiceDatabases:(PDSServiceDatabases *)serviceDatabases;
 
 @end
