20f780d3200677d06f2fe26a5bf89e7a3adf5f79
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-05-16 19:03:25 -0400

    docs: standardize Blob subsystem documentation

--- Garazyk/Sources/Blob/PDSDiskBlobProvider.h
+++ Garazyk/Sources/Blob/PDSDiskBlobProvider.h
@@ -5,10 +5,20 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+/**
+ * @abstract Implementation of PDSBlobProvider for local disk storage.
+ */
 @interface PDSDiskBlobProvider : NSObject <PDSBlobProvider>
 
+/**
+ * @abstract The base directory path where blobs are stored.
+ */
 @property (nonatomic, strong, readonly) NSURL *storageDirectory;
 
+/**
+ * @abstract Initializes a new local disk blob provider.
+ * @param storageDirectory The local URL path for blob persistence.
+ */
 - (instancetype)initWithStorageDirectory:(NSURL *)storageDirectory;
 
 @end
