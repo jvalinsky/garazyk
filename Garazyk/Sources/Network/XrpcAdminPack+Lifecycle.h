41900f2ee433f1f1b61c700e16ebb58dc20158e7
Jack Valinsky <jvalinsky@users.noreply.github.com>
2026-07-22 21:48:11 -0400

    refactor(network): decompose XrpcAdminPack.m into 5 category files

---
+++ Garazyk/Sources/Network/XrpcAdminPack+Lifecycle.h
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: 2025-2026 Jack Valinsky
+// SPDX-License-Identifier: Unlicense OR CC0-1.0
+
+#pragma once
+
+#import "Network/XrpcAdminPack.h"
+#import "Network/XrpcRoutePackServices.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XrpcAdminPack (Lifecycle)
++ (void)registerLifecycleEndpoints:(XrpcDispatcher *)dispatcher
+                services:(id<XrpcRoutePackServices>)services;
+@end
+
+NS_ASSUME_NONNULL_END
